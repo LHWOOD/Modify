da9570454a0e70c6a9a3e983fe9c840ee5f5bb18
LHWOOD <hutsonwood@gmail.com>
2021-06-02 12:46:15 -0600

    worked on get routes in User-Routes.js

--- controllers/Home-Routes.js
+++ controllers/Home-Routes.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 const { User, Music } = require("../Models");
 
+router.get("/", async (req, res) => {
+  res.render("homepage");
+});
+
 module.exports = router;
--- controllers/api/User-Routes.js
+++ controllers/api/User-Routes.js
@@ -1,4 +1,104 @@
 const router = require("express").Router();
 const { User } = require("../../Models");
 
+// get all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: {
+      exclude: ["password"],
+    },
+  })
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//get one user
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((userData) => {
+      if (!userData) {
+        res.status(404).json({
+          message: "No user found",
+        });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//create user
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    .then((userData) => {
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.username = userData.username;
+        req.session.loggIn = true;
+        res.json(userData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//login
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((userData) => {
+    if (!userData) {
+      res.status(400).json({
+        message: "No user with that usernam",
+      });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.username;
+      req.session.loggIn = true;
+      res.json({
+        user: userData,
+        message: "You are now logged in",
+      });
+    });
+
+    const validPassword = userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({
+        message: "Incorrect password",
+      });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.username;
+      req.session.loggIn = true;
+      res.json({
+        user: userData,
+        message: "You are now logged in",
+      });
+    });
+  });
+});
+
 module.exports = router;

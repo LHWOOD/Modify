dbfe3e34948a1b8ea7e105323c2e44d6ef104309
Altan Guc <altan.guc@colorado.edu>
2021-06-07 12:19:52 -0600

    heroku

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const userRoutes = require("./userRoutes");
-const dashboardRoutes = require("./dashboardRoute");
+const dashboardRoutes = require("./dashboardRoutes");
 // const dashboardRoutes = require("./dashboardRoute");
 
 router.use("/users", userRoutes);
